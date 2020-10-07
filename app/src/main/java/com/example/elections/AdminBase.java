be2eb62ca1a69662c09abfbd9f074628d7b0eef7
moahmed12samy <mohamedsamyabbas99@gmail.com>
2020-10-07 22:17:35 +0200

    integrating with firebase

---
+++ app/src/main/java/com/example/elections/AdminBase.java
@@ -0,0 +1,14 @@
+package com.example.elections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminBase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_base);
+    }
+}
